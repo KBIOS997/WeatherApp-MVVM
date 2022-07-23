ce2f3a205dd230e46ee710bf94db9fb8cec15695
senjaliya1989 <senjaliyavishal123@gmail.com>
2022-07-23 13:49:31 +0100

    Add files via upload

---
+++ WeatherApp MVVM/WeatherApp/WeatherApp/SupportingFiles/Utils/BaseServiceBoundary.swift
@@ -0,0 +1,15 @@
+//
+//  BaseServiceBoundary.swift
+//  WeatherApp
+//
+//  Created by Vishal on 08/07/22.
+//
+
+import Foundation
+
+typealias BoundaryFailureBlock = (_ error: String) -> Void
+typealias BoundaryEmptySuccess = () -> Void
+
+protocol BaseServiceBoundary {
+    
+}
